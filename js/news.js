f5a51327b935bc830bee23ce8733459fa2f9fa96
atnishat <nishat.mu47@gmail.com>
2022-09-03 22:38:13 +0600

    button added

--- js/news.js
+++ js/news.js
@@ -6,35 +6,31 @@ const allCategories = async () =>{
 }
 
 
-const displayCategories = linksItem =>{
-  // console.log(linksItem);
+const displayCategories = (linksItem) =>{
   const newDivField = document.getElementById('all-categories');
   linksItem.forEach(link => {
     const creatediv = document.createElement('ul');
     creatediv.classList.add('navbar-nav');
     creatediv.innerHTML = `
-
-    <li class="nav-item" id="full-news" onclick="somefunc('categoryId')">
-    <a class="nav-link active" aria-current="page" href="#">${link.category_name}</a>
-  </li>
-
-
-  `;
+    <li class="nav-item" id="full-news" >
+    <a class="nav-link active" aria-current="page"  onclick="showallNews('${link.category_id}')">${link.category_name}</a>
+  </li> `;
   newDivField.appendChild(creatediv);
 });
 }
 
 
-const showallNews = async() =>{
-    const url = `https://openapi.programming-hero.com/api/news/category/01`
+const showallNews = async(category_id) =>{
+    const url = ` https://openapi.programming-hero.com/api/news/category/${category_id}`
     const res = await fetch(url)
     const data = await res.json()
     displayAllNews(data.data)
 
 }
 const displayAllNews = news =>{
-    console.log(news);
+    // console.log(news);
     const newsItem = document.getElementById('show-all-news');
+    newsItem.textContent ='';
     news.forEach(allNewses =>{
         const createNewsDiv = document.createElement('div');
         createNewsDiv.classList.add('row');
@@ -56,11 +52,12 @@ const displayAllNews = news =>{
          <p><i class="arrow right"></i></p>
          </div>
          </div>
+         <button data-bs-toggle="modal" data-bs-target="#phoneDetailModal" class="bg-primary text-white">Show Details</button>
         </div>
       </div>
         `;
         newsItem.appendChild(createNewsDiv);
-
+       
         
     } )
 }
@@ -76,29 +73,7 @@ const displayAllNews = news =>{
 
 
 showallNews();
-
-
-
-
-
-
-
-
-
-
-
-
 allCategories();
 
 
 
-// {"status":true,"data":
-// [
-//   {"_id":"0282e0e58a5c404fbd15261f11c2ab6a",
-// "others_info":
-// {"is_todays_pick":false,"is_trending":true},
-// "category_id":"01","rating":{"number":4.5,"badge":"Excellent"},
-// "total_view":488,
-// "title":"Biden Pledges Nearly $3 Billion To Ukraine In Largest U.S. Military Aid Package Yet",
-// "author":{"name":"Jimmy Dane","published_date":"2022-08-24 17:27:34",
-// "img":"https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-
